26ea2cc7911ad94d9067f333430bbbeab5378090
Bawar <bawarmohamad100@gmail.com>
2025-09-24 13:24:25 +0300

    db seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Create test user for API authentication
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            EmployeePositionSeeder::class,
+            EmployeeSeeder::class,
         ]);
     }
 }
